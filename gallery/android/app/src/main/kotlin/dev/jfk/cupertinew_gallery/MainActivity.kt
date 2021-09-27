80ff1385efcc3d4726f2d60a47e6edefcaf35068
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2021-09-27 19:05:34 +0200

    chore: add melos and monorepo structure, rename org to dev.fluttercommunity

--- gallery/android/app/src/main/kotlin/dev/jfk/cupertinew_gallery/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.jfk.cupertinew_gallery
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ packages/cupertinew/gallery/android/app/src/main/kotlin/dev/jfk/cupertinew_gallery/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.fluttercommunity.cupertinew_gallery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
