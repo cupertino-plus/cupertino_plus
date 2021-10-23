d14d8e025afcd18b876bab551e9f6fb6f20f3f27
GroovinChip <groovinchip@gmail.com>
2021-10-23 13:16:35 -0400

    test: add debugFillProperties test for theme data

--- packages/cupertino_plus/test/theme/cupertino_plus_theme_data_test.dart
+++ packages/cupertino_plus/test/theme/cupertino_plus_theme_data_test.dart
@@ -1,4 +1,5 @@
 import 'package:cupertino_plus/cupertino_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -48,6 +49,28 @@ void main() {
 
       final theme = CupertinoPlusTheme.of(capturedContext);
       expect(theme.brightness, Brightness.dark);
+    });
+
+    testWidgets('default CupertinoPlusThemeData debugFillProperties',
+        (tester) async {
+      final builder = DiagnosticPropertiesBuilder();
+      const CupertinoPlusThemeData.raw(
+        colors: CupertinoPlusColors.light(),
+        brightness: Brightness.light,
+      ).debugFillProperties(builder);
+
+      final description = builder.properties
+          .where((node) => !node.isFiltered(DiagnosticLevel.info))
+          .map((node) => node.toString())
+          .toList();
+
+      expect(
+        description,
+        <String>[
+          '''colors: CupertinoPlusColors(Brightness.light, Color(0xffff3b30), Color(0xffff9500), Color(0xffffcc00), Color(0xff34c759), Color(0xff00c7be), Color(0xff30b0c7), Color(0xff32ade6), Color(0xff007aff), Color(0xff5856d6), Color(0xffaf52de), Color(0xffff2d55), Color(0xffa2845e), Color(0xff8e8e93), Color(0xffaeaeb2), Color(0xffc7c7cc), Color(0xffd1d1d6), Color(0xffe5e5ea), Color(0xfff2f2f7))''',
+          'brightness: light'
+        ],
+      );
     });
   });
 }
